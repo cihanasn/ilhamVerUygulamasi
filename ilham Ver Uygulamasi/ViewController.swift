22b987de0a1153ea85e9e9cc63a86f74bcaa42e5
cihanasn <cihanasan1@gmail.com>
2024-08-29 16:33:32 +0300

    Initial Commit

---
+++ ilham Ver Uygulamasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ilham Ver Uygulamasi
+//
+//  Created by Cihan AŞAN on 29.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
